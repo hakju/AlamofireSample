8bdeee82d32401b36af54390a2ec9146cdddf233
HakJu <gkrwnrh@gmail.com>
2021-08-03 10:33:09 +0900

    feat: fetchFilms 메서드 생성

--- AlamofireSample/ViewController.swift
+++ AlamofireSample/ViewController.swift
@@ -1,19 +1,19 @@
-//
-//  ViewController.swift
-//  AlamofireSample
-//
-//  Created by 이학주 on 2021/08/03.
-//
-
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchFilms()
     }
 
-
+    func fetchFilms() {
+        AF.request("https://swapi.dev/api/films")
+        .validate()
+        .responseDecodable(of: Films.self) { (response) in
+            guard let films = response.value else { return }
+            print(films.all[2].title)
+        }
+    }
 }
-
